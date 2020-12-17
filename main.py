f7e8e5672df85b2d81cbf9937b83e6299fa63a0c
syoukera <s.akira2986@gmail.com>
2020-12-17 11:56:27 +0900

    Increment functions to class

--- main.py
+++ main.py
@@ -97,195 +97,202 @@ class variables():
         self.xmonin = (self.SNS*self.densit*self.uwall*self.uwall).sum()
 
 
-def lisolv(istart, jstart, PHI, var):
-    
-    A = np.zeros(var.ny)
-    B = np.zeros(var.ny)
-    C = np.zeros(var.ny)
-    D = np.zeros(var.ny)
-    
-    jstm1 = jstart - 1
-    A[jstm1-1] = 0.0
-    
-    for i in range(istart-1, var.nim1):
-        C[jstm1-1] = PHI[i, jstm1-1]
+    def lisolv(self, istart, jstart, PHI):
         
-        for j in range(jstart-1, var.njm1):
-            A[j] = var.AN[i, j]
-            B[j] = var.AS[i, j]
-            C[j] = var.AE[i, j]*PHI[i+1, j] + var.AW[i, j]*PHI[i-1, j] + var.SU[i, j]
-            D[j] = var.AP[i, j]
+        A = np.zeros(self.ny)
+        B = np.zeros(self.ny)
+        C = np.zeros(self.ny)
+        D = np.zeros(self.ny)
+        
+        jstm1 = jstart - 1
+        A[jstm1-1] = 0.0
+        
+        for i in range(istart-1, self.nim1):
+            C[jstm1-1] = PHI[i, jstm1-1]
             
-            term = 1.0/(D[j] - B[j]*A[j-1])
-            A[j] = A[j]*term
-            C[j] = (C[j] + B[j]*C[j-1])*term
-            
-        for j in range(var.njm1-1, jstart-2, -1):
-                        
-            PHI[i, j] = A[j]*PHI[i, j+1] + C[j]
+            for j in range(jstart-1, self.njm1):
+                A[j] = self.AN[i, j]
+                B[j] = self.AS[i, j]
+                C[j] = self.AE[i, j]*PHI[i+1, j] + self.AW[i, j]*PHI[i-1, j] + self.SU[i, j]
+                D[j] = self.AP[i, j]
+                
+                term = 1.0/(D[j] - B[j]*A[j-1])
+                A[j] = A[j]*term
+                C[j] = (C[j] + B[j]*C[j-1])*term
+                
+            for j in range(self.njm1-1, jstart-2, -1):
+                            
+                PHI[i, j] = A[j]*PHI[i, j+1] + C[j]
 
 
-def calcu(var):
-    
-    for i in range(2, var.nim1):
-        for j in range(1, var.njm1):
-            cn = 0.5*var.densit*(var.V[i  , j+1] + var.V[i-1, j+1])*var.SEWU[i]
-            cs = 0.5*var.densit*(var.V[i  , j  ] + var.V[i-1, j  ])*var.SEWU[i]
-            ce = 0.5*var.densit*(var.U[i+1, j  ] + var.U[i  , j  ])*var.SNS[j]
-            cw = 0.5*var.densit*(var.U[i  , j  ] + var.U[i-1, j  ])*var.SNS[j]
-            dn = var.viscos*var.SEWU[i]/var.DYNP[j]
-            ds = var.viscos*var.SEWU[i]/var.DYPS[j]
-            de = var.viscos*var.SNS[j]/var.DXEPU[i]
-            dw = var.viscos*var.SNS[j]/var.DXPWU[i]
-            var.AN[i, j] = max(abs(0.5*cn), dn) - 0.5*cn
-            var.AS[i, j] = max(abs(0.5*cs), ds) + 0.5*cs
-            var.AE[i, j] = max(abs(0.5*ce), de) - 0.5*ce
-            var.AW[i, j] = max(abs(0.5*cw), dw) + 0.5*cw
-            var.DU[i, j] = var.SNS[j]
-            var.SU[i, j] = var.DU[i, j]*(var.P[i-1, j] - var.P[i, j])
-            var.SP[i, j] = 0.0
-            
-    yp = var.YV[-1] - var.Y[-2]
-    j = var.njm1-1
-    for i in range(2, var.nim1):
-        tmult = var.viscos/yp
-        var.SP[i, j] = var.SP[i, j] - tmult*var.SEWU[i]
-        var.SU[i, j] = var.SU[i, j] + tmult*var.SEWU[i]*var.U[i, j+1]
-        var.AN[i, j] = 0.0
+    def calcu(self):
         
-    yp = var.Y[1] - var.YV[1]
-    j = 1
-    for i in range(2, var.nim1):
-        tmult = var.viscos/yp
-        var.SP[i, j] = var.SP[i, j] - tmult*var.SEWU[i]
-        var.SU[i, j] = var.SU[i, j] + tmult*var.SEWU[i]*var.U[i, j-1]
-        var.AS[i, j] = 0.0
-        
-    for j in range(1, var.njm1):
-        var.AE[-2, j] = 0.0
-        var.AW[2, j] = 0.0
-        
-    var.resoru = 0.0
-    for i in range(2, var.nim1):
-        for j in range(1, var.njm1):
-            var.AP[i, j] = var.AN[i, j] + var.AS[i, j] + var.AE[i, j] + var.AW[i, j]                          - var.SP[i, j]
-            var.DU[i, j] = var.DU[i, j]/var.AP[i, j]
-            resor = var.AN[i, j]*var.U[i  , j+1] + var.AS[i, j]*var.U[i  , j-1]                   + var.AE[i, j]*var.U[i+1, j  ] + var.AW[i, j]*var.U[i-1, j  ]                   - var.AP[i, j]*var.U[i  , j  ] + var.SU[i, j]
-            var.resoru += abs(resor)
+        for i in range(2, self.nim1):
+            for j in range(1, self.njm1):
+                cn = 0.5*self.densit*(self.V[i  , j+1] + self.V[i-1, j+1])*self.SEWU[i]
+                cs = 0.5*self.densit*(self.V[i  , j  ] + self.V[i-1, j  ])*self.SEWU[i]
+                ce = 0.5*self.densit*(self.U[i+1, j  ] + self.U[i  , j  ])*self.SNS[j]
+                cw = 0.5*self.densit*(self.U[i  , j  ] + self.U[i-1, j  ])*self.SNS[j]
+                dn = self.viscos*self.SEWU[i]/self.DYNP[j]
+                ds = self.viscos*self.SEWU[i]/self.DYPS[j]
+                de = self.viscos*self.SNS[j]/self.DXEPU[i]
+                dw = self.viscos*self.SNS[j]/self.DXPWU[i]
+                self.AN[i, j] = max(abs(0.5*cn), dn) - 0.5*cn
+                self.AS[i, j] = max(abs(0.5*cs), ds) + 0.5*cs
+                self.AE[i, j] = max(abs(0.5*ce), de) - 0.5*ce
+                self.AW[i, j] = max(abs(0.5*cw), dw) + 0.5*cw
+                self.DU[i, j] = self.SNS[j]
+                self.SU[i, j] = self.DU[i, j]*(self.P[i-1, j] - self.P[i, j])
+                self.SP[i, j] = 0.0
+                
+        yp = self.YV[-1] - self.Y[-2]
+        j = self.njm1-1
+        for i in range(2, self.nim1):
+            tmult = self.viscos/yp
+            self.SP[i, j] = self.SP[i, j] - tmult*self.SEWU[i]
+            self.SU[i, j] = self.SU[i, j] + tmult*self.SEWU[i]*self.U[i, j+1]
+            self.AN[i, j] = 0.0
             
-            # under relaxation
-            var.AP[i, j] = var.AP[i, j]/var.urfu
-            var.SU[i, j] = var.SU[i, j] + (1.0 - var.urfu)*var.AP[i, j]*var.U[i, j]
-            var.DU[i, j] = var.DU[i, j]*var.urfu
+        yp = self.Y[1] - self.YV[1]
+        j = 1
+        for i in range(2, self.nim1):
+            tmult = self.viscos/yp
+            self.SP[i, j] = self.SP[i, j] - tmult*self.SEWU[i]
+            self.SU[i, j] = self.SU[i, j] + tmult*self.SEWU[i]*self.U[i, j-1]
+            self.AS[i, j] = 0.0
             
-    for i in range(var.nswpu):
-        lisolv(3, 2, var.U, var)      
+        for j in range(1, self.njm1):
+            self.AE[-2, j] = 0.0
+            self.AW[2, j] = 0.0
+            
+        self.resoru = 0.0
+        for i in range(2, self.nim1):
+            for j in range(1, self.njm1):
+                self.AP[i, j] = self.AN[i, j] + self.AS[i, j] + self.AE[i, j] + self.AW[i, j] \
+                              - self.SP[i, j]
+                self.DU[i, j] = self.DU[i, j]/self.AP[i, j]
+                resor = self.AN[i, j]*self.U[i  , j+1] + self.AS[i, j]*self.U[i  , j-1] \
+                      + self.AE[i, j]*self.U[i+1, j  ] + self.AW[i, j]*self.U[i-1, j  ] \
+                      - self.AP[i, j]*self.U[i  , j  ] + self.SU[i, j]
+                self.resoru += abs(resor)
+                
+                # under relaxation
+                self.AP[i, j] = self.AP[i, j]/self.urfu
+                self.SU[i, j] = self.SU[i, j] + (1.0 - self.urfu)*self.AP[i, j]*self.U[i, j]
+                self.DU[i, j] = self.DU[i, j]*self.urfu
+                
+        for i in range(self.nswpu):
+            self.lisolv(3, 2, self.U)      
 
 
-def calcv(var):
-    
-    for i in range(1, var.nim1):
-        for j in range(2, var.njm1):
-            cn = 0.5*var.densit*(var.V[i  , j+1] + var.V[i  , j  ])*var.SEW[i]
-            cs = 0.5*var.densit*(var.V[i  , j  ] + var.V[i  , j-1])*var.SEW[i]
-            ce = 0.5*var.densit*(var.U[i+1, j  ] + var.U[i+1, j-1])*var.SNSV[j]
-            cw = 0.5*var.densit*(var.U[i  , j  ] + var.U[i  , j-1])*var.SNSV[j]
-            dn = var.viscos*var.SEW[i]/var.DYNPV[j]
-            ds = var.viscos*var.SEW[i]/var.DYPSV[j]
-            de = var.viscos*var.SNSV[j]/var.DXEP[i]
-            dw = var.viscos*var.SNSV[j]/var.DXPW[i]
-            var.AN[i, j] = max(abs(0.5*cn), dn) - 0.5*cn
-            var.AS[i, j] = max(abs(0.5*cs), ds) + 0.5*cs
-            var.AE[i, j] = max(abs(0.5*ce), de) - 0.5*ce
-            var.AW[i, j] = max(abs(0.5*cw), dw) + 0.5*cw
-            var.DV[i, j] = var.SEW[i]
-            var.SU[i, j] = var.DV[i, j]*(var.P[i, j-1] - var.P[i, j])
-            var.SP[i, j] = 0.0
-            
-    # West wall
-    xp = var.X[1] - var.XU[1]
-    i = 1
-    for j in range(2, var.njm1):
-        tmult = var.viscos/xp
-        var.SP[i, j] = var.SP[i, j] - tmult*var.SNSV[j]
-        var.SU[i, j] = var.SU[i, j] + tmult*var.SNSV[j]*var.V[i-1, j]
-        var.AW[i, j] = 0.0
+    def calcv(self):
         
-    # East wall
-    xp = var.XU[-1] - var.X[-2]
-    i = var.nim1 - 1
-    for j in range(2, var.njm1):
-        tmult = var.viscos/xp
-        var.SP[i, j] = var.SP[i, j] - tmult*var.SNSV[j]
-        var.SU[i, j] = var.SU[i, j] + tmult*var.SNSV[j]*var.V[i+1, j]
-        var.AE[i, j] = 0.0
-        
-    # Top and bottom wall
-    for i in range(1, var.nim1):
-        var.AS[i, 2] = 0.0
-        var.AN[i, -2] = 0.0
-        
-    var.resorv = 0.0
-    for i in range(1, var.nim1):
-        for j in range(2, var.njm1):
-            var.AP[i, j] = var.AN[i, j] + var.AS[i, j] + var.AE[i, j] + var.AW[i, j]                          - var.SP[i, j]
-            var.DV[i, j] = var.DV[i, j]/var.AP[i, j]
-            resor = var.AN[i, j]*var.V[i  , j+1] + var.AS[i, j]*var.V[i  , j-1]                   + var.AE[i, j]*var.V[i+1, j  ] + var.AW[i, j]*var.V[i-1, j  ]                   - var.AP[i, j]*var.V[i  , j  ] + var.SU[i, j]
-            var.resorv += abs(resor)
+        for i in range(1, self.nim1):
+            for j in range(2, self.njm1):
+                cn = 0.5*self.densit*(self.V[i  , j+1] + self.V[i  , j  ])*self.SEW[i]
+                cs = 0.5*self.densit*(self.V[i  , j  ] + self.V[i  , j-1])*self.SEW[i]
+                ce = 0.5*self.densit*(self.U[i+1, j  ] + self.U[i+1, j-1])*self.SNSV[j]
+                cw = 0.5*self.densit*(self.U[i  , j  ] + self.U[i  , j-1])*self.SNSV[j]
+                dn = self.viscos*self.SEW[i]/self.DYNPV[j]
+                ds = self.viscos*self.SEW[i]/self.DYPSV[j]
+                de = self.viscos*self.SNSV[j]/self.DXEP[i]
+                dw = self.viscos*self.SNSV[j]/self.DXPW[i]
+                self.AN[i, j] = max(abs(0.5*cn), dn) - 0.5*cn
+                self.AS[i, j] = max(abs(0.5*cs), ds) + 0.5*cs
+                self.AE[i, j] = max(abs(0.5*ce), de) - 0.5*ce
+                self.AW[i, j] = max(abs(0.5*cw), dw) + 0.5*cw
+                self.DV[i, j] = self.SEW[i]
+                self.SU[i, j] = self.DV[i, j]*(self.P[i, j-1] - self.P[i, j])
+                self.SP[i, j] = 0.0
+                
+        # West wall
+        xp = self.X[1] - self.XU[1]
+        i = 1
+        for j in range(2, self.njm1):
+            tmult = self.viscos/xp
+            self.SP[i, j] = self.SP[i, j] - tmult*self.SNSV[j]
+            self.SU[i, j] = self.SU[i, j] + tmult*self.SNSV[j]*self.V[i-1, j]
+            self.AW[i, j] = 0.0
             
-            # under relaxation
-            var.AP[i, j] = var.AP[i, j]/var.urfv
-            var.SU[i, j] = var.SU[i, j] + (1.0 - var.urfv)*var.AP[i, j]*var.V[i, j]
-            var.DV[i, j] = var.DV[i, j]*var.urfv
+        # East wall
+        xp = self.XU[-1] - self.X[-2]
+        i = self.nim1 - 1
+        for j in range(2, self.njm1):
+            tmult = self.viscos/xp
+            self.SP[i, j] = self.SP[i, j] - tmult*self.SNSV[j]
+            self.SU[i, j] = self.SU[i, j] + tmult*self.SNSV[j]*self.V[i+1, j]
+            self.AE[i, j] = 0.0
             
-    for i in range(var.nswpv):
-        lisolv(2, 3, var.V, var)
+        # Top and bottom wall
+        for i in range(1, self.nim1):
+            self.AS[i, 2] = 0.0
+            self.AN[i, -2] = 0.0
+            
+        self.resorv = 0.0
+        for i in range(1, self.nim1):
+            for j in range(2, self.njm1):
+                self.AP[i, j] = self.AN[i, j] + self.AS[i, j] + self.AE[i, j] + self.AW[i, j] \
+                              - self.SP[i, j]
+                self.DV[i, j] = self.DV[i, j]/self.AP[i, j]
+                resor = self.AN[i, j]*self.V[i  , j+1] + self.AS[i, j]*self.V[i  , j-1] \
+                      + self.AE[i, j]*self.V[i+1, j  ] + self.AW[i, j]*self.V[i-1, j  ] \
+                      - self.AP[i, j]*self.V[i  , j  ] + self.SU[i, j]
+                self.resorv += abs(resor)
+                
+                # under relaxation
+                self.AP[i, j] = self.AP[i, j]/self.urfv
+                self.SU[i, j] = self.SU[i, j] + (1.0 - self.urfv)*self.AP[i, j]*self.V[i, j]
+                self.DV[i, j] = self.DV[i, j]*self.urfv
+                
+        for i in range(self.nswpv):
+            self.lisolv(2, 3, self.V)
 
 
-def calcp(var):
-    
-    var.resorm = 0.0
-    
-    for i in range(1, var.nim1):
-        for j in range(1, var.njm1):
-            var.AN[i, j] = var.densit*var.SEW[i]*var.DV[i  , j+1]
-            var.AS[i, j] = var.densit*var.SEW[i]*var.DV[i  , j  ]
-            var.AE[i, j] = var.densit*var.SNS[j]*var.DU[i+1, j  ]
-            var.AW[i, j] = var.densit*var.SNS[j]*var.DU[i  , j  ]
-            
-            cn = var.densit*var.V[i  , j+1]*var.SEW[i]
-            cs = var.densit*var.V[i  , j  ]*var.SEW[i]
-            ce = var.densit*var.U[i+1, j  ]*var.SNS[j]
-            cw = var.densit*var.U[i  , j  ]*var.SNS[j]
-            smp = cn - cs + ce - cw
-            
-            var.SP[i, j] = 0.0
-            var.SU[i, j] = - smp
-            
-            var.resorm += abs(smp)
+    def calcp(self):
         
-    for i in range(1, var.nim1):
-        for j in range(1, var.njm1):
-            var.AP[i, j] = var.AN[i, j] + var.AS[i, j] + var.AE[i, j] + var.AW[i, j]                          - var.SP[i, j]
+        self.resorm = 0.0
+        
+        for i in range(1, self.nim1):
+            for j in range(1, self.njm1):
+                self.AN[i, j] = self.densit*self.SEW[i]*self.DV[i  , j+1]
+                self.AS[i, j] = self.densit*self.SEW[i]*self.DV[i  , j  ]
+                self.AE[i, j] = self.densit*self.SNS[j]*self.DU[i+1, j  ]
+                self.AW[i, j] = self.densit*self.SNS[j]*self.DU[i  , j  ]
+                
+                cn = self.densit*self.V[i  , j+1]*self.SEW[i]
+                cs = self.densit*self.V[i  , j  ]*self.SEW[i]
+                ce = self.densit*self.U[i+1, j  ]*self.SNS[j]
+                cw = self.densit*self.U[i  , j  ]*self.SNS[j]
+                smp = cn - cs + ce - cw
+                
+                self.SP[i, j] = 0.0
+                self.SU[i, j] = - smp
+                
+                self.resorm += abs(smp)
             
-    var.PP *= 0.0
-            
-    for i in range(var.nswpp):
-        lisolv(2, 2, var.PP, var)      
+        for i in range(1, self.nim1):
+            for j in range(1, self.njm1):
+                self.AP[i, j] = self.AN[i, j] + self.AS[i, j] + self.AE[i, j] + self.AW[i, j] \
+                              - self.SP[i, j]
+                
+        self.PP *= 0.0
+                
+        for i in range(self.nswpp):
+            self.lisolv(2, 2, self.PP)      
 
-    for i in range(1, var.nim1):
-        for j in range(1, var.njm1):
-            if i != 1: 
-                var.U[i, j] = var.U[i, j] + var.DU[i, j]*(var.PP[i-1, j  ] - var.PP[i, j])
-            if j != 1:
-                var.V[i, j] = var.V[i, j] + var.DV[i, j]*(var.PP[i  , j-1] - var.PP[i, j])
-            
-    ppref = var.PP[var.ipref-1, var.jpref-1]
-    
-    for i in range(1, var.nim1):
-        for j in range(1, var.njm1):
-            var.P[i, j] = var.P[i, j] + var.urfp*(var.PP[i, j] - ppref)
-#             var.PP[i, j] = 0.0
+        for i in range(1, self.nim1):
+            for j in range(1, self.njm1):
+                if i != 1: 
+                    self.U[i, j] = self.U[i, j] + self.DU[i, j]*(self.PP[i-1, j  ] - self.PP[i, j])
+                if j != 1:
+                    self.V[i, j] = self.V[i, j] + self.DV[i, j]*(self.PP[i  , j-1] - self.PP[i, j])
+                
+        ppref = self.PP[self.ipref-1, self.jpref-1]
+        
+        for i in range(1, self.nim1):
+            for j in range(1, self.njm1):
+                self.P[i, j] = self.P[i, j] + self.urfp*(self.PP[i, j] - ppref)
+    #             self.PP[i, j] = 0.0
 
 def main():
     var = variables()
@@ -298,9 +305,9 @@ def main():
 
     for niter in range(maxit):
 
-        calcu(var)
-        calcv(var)
-        calcp(var)
+        var.calcu()
+        var.calcv()
+        var.calcp()
 
         resorm = var.resorm/var.flowin
         resoru = var.resoru/var.xmonin
@@ -318,5 +325,5 @@ def main():
         if niter == maxit-1:
             print('Not converged...')
 
-if __name__ is '__main__':
+if __name__ == '__main__':
     main()
